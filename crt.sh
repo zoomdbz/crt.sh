95e9f6ab6edb546f45b54b4d91c07e0984e0e814
zoomdbz <kali@kali>
2022-07-02 20:28:34 -0400

    modified:   crt.sh parellel curling for speed.

--- crt.sh
+++ crt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in `cat ${1+"$@"} 2>/dev/null || echo ${1+"$@"}`;    do curl -s "https://crt.sh/?q=%25.$i&output=json" | jq -r ".[].name_value" | sed "s/*.//g" | sort -u ; done
+for i in `cat ${1+"$@"} 2>/dev/null || echo ${1+"$@"}`;	do curl --parallel --parallel-immediate --parallel-max 12 -s "https://crt.sh/?q=.$i&output=json" | jq -r ".[].name_value" | sed "s/*.//g" | sort -u ; done
